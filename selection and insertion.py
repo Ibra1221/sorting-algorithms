4b5b3b574777e49a0d1a8c1f028d1289cff60f1b
Youssef hesham <capoyoussef16@gmail.com>
2026-02-26 20:10:01 +0200

    add selection and insertion

---
+++ selection and insertion.py
@@ -0,0 +1,24 @@
+def Selection_Sort(arr,n):
+    for i in range(n-1):
+        i_min=i
+        for j in range(i+1,n):
+            if arr[j]<arr[i_min]:
+                i_min=j
+                
+
+        if(i!=i_min):
+         arr[i],arr[i_min]=arr[i_min],arr[i]
+    return arr
+def insertion_sort(arr,n):
+   for i in range(1,n):
+      key=arr[i]
+      j=i
+      while(j>0 and arr[j-1]>key):
+         arr[j]=arr[j-1]
+         j-=1  
+         arr[j]=key 
+
+
+arr = [39, 10, 14, 40, 13]
+insertion_sort(arr, len(arr))
+print(f"sorted list {arr}")
