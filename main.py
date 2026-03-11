3821619e8f57d31f2fe1ded0ffe9c98e4bbcb66c
Abdelrahman Mohamed <abdelrahman20205050@gmail.com>
2026-03-11 21:21:50 +0200

    graph updated

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sorting2 import quick_sort, heap_sort, merge_sort
 import matplotlib.pyplot as plt
 
 
-sizes = [1000, 2500, 5000,10000, 25000]
+sizes = [1000,5000,10000,20000,30000]
 
 insertion_times = []
 selection_times = []
